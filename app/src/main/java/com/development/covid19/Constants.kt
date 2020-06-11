17b4ed776213f94f18b40933e98538b3dd9e92f9
matildacheruu <matildacheruu@gmail.com>
2020-06-11 05:31:36 +0300

    COVID19 APP

---
+++ app/src/main/java/com/development/covid19/Constants.kt
@@ -0,0 +1,13 @@
+package com.development.covid19
+
+
+class Constants {
+
+    companion object {
+        const val COVID_WORLD_MAP_URL = "https://www.google.com/covid19-map/"
+        const val WHO_URL = "https://www.who.int/health-topics/coronavirus"
+        const val INFO_RESOURCES_URL = "https://www.google.com/covid19/"
+        const val KENYA_URL = "https://www.covid19kenya.org/"
+        const val NEWS_URL = "https://www.nation.co.ke/coronavirus"
+    }
+}
